10805b798bd0884af1c1d8d3c1ada94d956fa531
Jim <JimFuhrman@Yahoo.com>
2022-08-23 09:06:27 -0500

    got it successfully loading a row of small frames

--- bowlingScoresFile.js
+++ bowlingScoresFile.js
@@ -1,2 +1,4 @@
-export const AngelasBowlingFrames = [[8, 1], [9, "/"], [9, "/"], ["x"], [4, 5], [1, "/"], ["x"], ["x"], [3, "/"], [4, "/", 8]]
-export const BriansBowlingFrames = [["x"], ["x"], ["x"], ["x"], ["x"], ["x"], ["x"], ["x"], ["x"], ["x", 9, "/"]]
+// export const AngelasBowlingFrames = [[8, 1], [9, "/"], [9, "/"], ["x"], [4, 5], [1, "/"], ["x"], ["x"], [3, "/"], [4, "/", 8]]
+// export const BriansBowlingFrames = [["x"], ["x"], ["x"], ["x"], ["x"], ["x"], ["x"], ["x"], ["x"], ["x", 9, "/"]]
+export const CharliesBowlingFrames = [[5, "/"], ["X", ""], [9, "-"], ["x", ""], ["x", ""], ["x", "🦃"], [9, "/"], [8, "/"], [6, 2], ["x", 8, "/"]]
+// 20 39 48 78 107 127 145 161 169 189
--- main.js
+++ main.js
@@ -2,10 +2,22 @@ import "./style.scss"
 import * as scoresFile from './bowlingScoresFile'
 
 const bowlingFrames1 = document.querySelector('.bowlingFrames');
-let totalScore = 0;
+const largeFrames = document.querySelectorAll('.bowlingFrame--large');
+const bowlers = document.querySelectorAll('.bowlers');
+const scores = scoresFile.CharliesBowlingFrames;
 
-function score(e) {
-  console.log(`this: ${this}`)
-}
-console.log(bowlingFrames1)
-// bowlingFrames1.addEventListener("keydown", score.bind(this))
+scores.forEach((score, idx, arr) => {
+  console.log(`score: ${score}`)
+  let frameNmbr = idx + 1;
+  let frameName = ".frame"+String(frameNmbr)  /* frameNames will be .frame1, .frame2, etc. */
+
+  score.forEach((smallFrame, sfIdx, sfarr) => {
+/* clsList will be ".frame1.bowlingFrame--small", then ".frame2.bowlingFrame--small" */
+    let clsList = frameName + ".bowlingFrame--small";  
+    let currentFrame = document.querySelectorAll(clsList);
+    currentFrame[sfIdx].textContent = smallFrame
+    
+    console.log(`smallFrame = ${smallFrame}`)
+    console.log(`currentFrame[sfIdx].textContent: ${currentFrame[sfIdx].textContent}`)
+  })
+}) 

728e971abc04ba3ef0849fc3d0902c98c52fe1f7
Jim <JimFuhrman@Yahoo.com>
2022-08-23 13:58:54 -0500

    successfully added 10 individual score frames

--- bowlingScoresFile.js
+++ bowlingScoresFile.js
@@ -1,4 +1,10 @@
+/*
+ Right now each line of Bowling is an array consisting of --
+ 1) Bowler name
+ 2) an array of arrays. These are the 2 small frames within frames 1-9 and 3 small frames in frame 10.
+ 3) an array of frame scores.  These are the 10 individual scores for the 10 individual frames. 
+*/
+
 // export const AngelasBowlingFrames = [[8, 1], [9, "/"], [9, "/"], ["x"], [4, 5], [1, "/"], ["x"], ["x"], [3, "/"], [4, "/", 8]]
-// export const BriansBowlingFrames = [["x"], ["x"], ["x"], ["x"], ["x"], ["x"], ["x"], ["x"], ["x"], ["x", 9, "/"]]
-export const CharliesBowlingFrames = [[5, "/"], ["X", ""], [9, "-"], ["x", ""], ["x", ""], ["x", "🦃"], [9, "/"], [8, "/"], [6, 2], ["x", 8, "/"]]
-// 20 39 48 78 107 127 145 161 169 189
+// export const CharliesBowlingFrames = [["x"], ["x"], ["x"], ["x"], ["x"], ["x"], ["x"], ["x"], ["x"], ["x", 9, "/"]]
+export const BriansBowlingFrames = ["Brian Buhrman", [[5, "/"], ["X", ""], [9, "-"], ["x", ""], ["x", ""], ["x", "🦃"], [9, "/"], [8, "/"], [6, 2], ["x", 8, "/"]], [20, 39, 48, 78, 107, 127, 145, 161, 169, 189]]
--- main.js
+++ main.js
@@ -1,23 +1,33 @@
 import "./style.scss"
 import * as scoresFile from './bowlingScoresFile'
 
-const bowlingFrames1 = document.querySelector('.bowlingFrames');
+// const bowlingFrames1 = document.querySelector('.bowlingFrames');
 const largeFrames = document.querySelectorAll('.bowlingFrame--large');
-const bowlers = document.querySelectorAll('.bowlers');
-const scores = scoresFile.CharliesBowlingFrames;
+const bowlerName = document.querySelectorAll('.bowlerName');
+const scores = scoresFile.BriansBowlingFrames;
 
-scores.forEach((score, idx, arr) => {
-  console.log(`score: ${score}`)
+const bowler = scores[0];
+const frames = scores[1];
+const frameScores = scores[2];
+
+console.log(`bowler: ${bowler}`)
+console.log(`frames: ${frames}`)
+console.log(`frameScores: ${frameScores}`)
+
+bowlerName[0].textContent = bowler;
+frames.forEach((frame, idx, arr) => {
+  // console.log(`frame: ${frame}`)
   let frameNmbr = idx + 1;
   let frameName = ".frame"+String(frameNmbr)  /* frameNames will be .frame1, .frame2, etc. */
+  largeFrames[idx].textContent = frameScores[idx];
 
-  score.forEach((smallFrame, sfIdx, sfarr) => {
+  frame.forEach((smallFrame, sfIdx, sfarr) => {
 /* clsList will be ".frame1.bowlingFrame--small", then ".frame2.bowlingFrame--small" */
     let clsList = frameName + ".bowlingFrame--small";  
     let currentFrame = document.querySelectorAll(clsList);
     currentFrame[sfIdx].textContent = smallFrame
     
-    console.log(`smallFrame = ${smallFrame}`)
-    console.log(`currentFrame[sfIdx].textContent: ${currentFrame[sfIdx].textContent}`)
+    // console.log(`smallFrame = ${smallFrame}`)
+    // console.log(`currentFrame[sfIdx].textContent: ${currentFrame[sfIdx].textContent}`)
   })
 }) 

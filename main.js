0c833059a7cb41143a30e2478b59a2d65134f490
Jim <JimFuhrman@Yahoo.com>
2022-08-24 11:04:34 -0500

    made it work for multiple bowlers

--- main.js
+++ main.js
@@ -1,33 +1,63 @@
 import "./style.scss"
 import * as scoresFile from './bowlingScoresFile'
 
-// const bowlingFrames1 = document.querySelector('.bowlingFrames');
+const bowlingTeam = document.querySelector('.bowlers');
 const largeFrames = document.querySelectorAll('.bowlingFrame--large');
 const bowlerName = document.querySelectorAll('.bowlerName');
-const scores = scoresFile.BriansBowlingFrames;
+const bowlers = scoresFile.allBowlingFrames;
 
-const bowler = scores[0];
-const frames = scores[1];
-const frameScores = scores[2];
+bowlers.forEach((bowler, bIdx, bArr) => {
+  let personName = bowler[0];
+  let frames = bowler[1];
+  let frameScores = bowler[2];
 
-console.log(`bowler: ${bowler}`)
-console.log(`frames: ${frames}`)
-console.log(`frameScores: ${frameScores}`)
+  /*
+  console.log(`personName: ${personName}`)
+  console.log(`frames: ${frames}`)
+  console.log(`frameScores: ${frameScores}`)
+  console.log(' ')
+  console.log(`frames[1][0]: ${frames[1][0]}`)
+  */
 
-bowlerName[0].textContent = bowler;
-frames.forEach((frame, idx, arr) => {
-  // console.log(`frame: ${frame}`)
-  let frameNmbr = idx + 1;
-  let frameName = ".frame"+String(frameNmbr)  /* frameNames will be .frame1, .frame2, etc. */
-  largeFrames[idx].textContent = frameScores[idx];
-
-  frame.forEach((smallFrame, sfIdx, sfarr) => {
-/* clsList will be ".frame1.bowlingFrame--small", then ".frame2.bowlingFrame--small" */
-    let clsList = frameName + ".bowlingFrame--small";  
-    let currentFrame = document.querySelectorAll(clsList);
-    currentFrame[sfIdx].textContent = smallFrame
-    
-    // console.log(`smallFrame = ${smallFrame}`)
-    // console.log(`currentFrame[sfIdx].textContent: ${currentFrame[sfIdx].textContent}`)
-  })
-}) 
+  let newBowler = document.createElement("div");
+  bowlingTeam.appendChild(newBowler);
+  newBowler.classList.add('bowler');
+  newBowler.innerHTML = `
+  <div class="bowlerName">${personName}</div>
+    <div class="bowlingFramesRow--small">
+      <div class="frame1 bowlingFrame--small">${frames[0][0]}</div>
+      <div class="frame1 bowlingFrame--small">${frames[0][1]}</div>
+      <div class="frame2 bowlingFrame--small">${frames[1][0]}</div>
+      <div class="frame2 bowlingFrame--small">${frames[1][1]}</div>
+      <div class="frame3 bowlingFrame--small">${frames[2][0]}</div>
+      <div class="frame3 bowlingFrame--small">${frames[2][1]}</div>
+      <div class="frame4 bowlingFrame--small">${frames[3][0]}</div>
+      <div class="frame4 bowlingFrame--small">${frames[3][1]}</div>
+      <div class="frame5 bowlingFrame--small">${frames[4][0]}</div>
+      <div class="frame5 bowlingFrame--small">${frames[4][1]}</div>
+      <div class="frame6 bowlingFrame--small">${frames[5][0]}</div>
+      <div class="frame6 bowlingFrame--small">${frames[5][1]}</div>
+      <div class="frame7 bowlingFrame--small">${frames[6][0]}</div>
+      <div class="frame7 bowlingFrame--small">${frames[6][1]}</div>
+      <div class="frame8 bowlingFrame--small">${frames[7][0]}</div>
+      <div class="frame8 bowlingFrame--small">${frames[7][1]}</div>
+      <div class="frame9 bowlingFrame--small">${frames[8][0]}</div>
+      <div class="frame9 bowlingFrame--small">${frames[8][1]}</div>
+      <div class="frame10 bowlingFrame--small">${frames[9][0]}</div>
+      <div class="frame10 bowlingFrame--small">${frames[9][1]}</div>
+      <div class="frame10 bowlingFrame--small">${frames[9][2]}</div>
+    </div>
+    <div class="bowlingFramesRow--large">
+      <div class="frame1 bowlingFrame--large">${frameScores[0]}</div>
+      <div class="frame2 bowlingFrame--large">${frameScores[1]}</div>
+      <div class="frame3 bowlingFrame--large">${frameScores[2]}</div>
+      <div class="frame4 bowlingFrame--large">${frameScores[3]}</div>
+      <div class="frame5 bowlingFrame--large">${frameScores[4]}</div>
+      <div class="frame6 bowlingFrame--large">${frameScores[5]}</div>
+      <div class="frame7 bowlingFrame--large">${frameScores[6]}</div>
+      <div class="frame8 bowlingFrame--large">${frameScores[7]}</div>
+      <div class="frame9 bowlingFrame--large">${frameScores[8]}</div>
+      <div class="frame10 bowlingFrame--large">${frameScores[9]}</div>
+    </div>
+  `
+})
